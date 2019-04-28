6ed83f414fa080c25a90b04fd7764fe08daba845
EricPwg <dz92286@gmail.com>
2019-04-29 00:48:34 +0800

    debounce

--- Serial_sample/Serial_sample.ino
+++ Serial_sample/Serial_sample.ino
@@ -1,4 +1,5 @@
 int get_t;
+bool flag;
 
 void get_serial(int get_t){
   Serial.println(get_t);
@@ -7,16 +8,21 @@ void get_serial(int get_t){
 void setup() {
   Serial.begin(9600);
   get_t = 0;
+  flag = false;
 }
 
 void loop() {
   if (Serial.available()){
     char get_char = Serial.read();
     if (get_char == ' ' || get_char == '\n' || get_char == '\r'){
-      get_serial(get_t);
-      get_t = 0;
+      if (flag){
+        flag = false;
+        get_serial(get_t);
+        get_t = 0;
+      }
    }
    else{
+        flag = true;
         get_t = get_t*10+((int)get_char-48);
    }
   }
