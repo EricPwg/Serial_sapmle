8c7d81d6bb072e84994d2929708bc7045cc88bab
EricPwg <dz92286@gmail.com>
2019-04-29 00:46:34 +0800

    handle string

--- Serial_sample/Serial_sample.ino
+++ Serial_sample/Serial_sample.ino
@@ -1,11 +1,23 @@
+int get_t;
+
+void get_serial(int get_t){
+  Serial.println(get_t);
+}
+
 void setup() {
   Serial.begin(9600);
-
+  get_t = 0;
 }
 
 void loop() {
-  if(Serial.available()){
-      int a = Serial.read();
-      Serial.println(a);
+  if (Serial.available()){
+    char get_char = Serial.read();
+    if (get_char == ' ' || get_char == '\n' || get_char == '\r'){
+      get_serial(get_t);
+      get_t = 0;
+   }
+   else{
+        get_t = get_t*10+((int)get_char-48);
+   }
   }
 }
