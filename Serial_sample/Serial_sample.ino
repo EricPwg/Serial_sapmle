c07ea996e2d8f4d198e79bfb0215b281254201c6
EricPwg <dz92286@gmail.com>
2019-04-29 00:49:53 +0800

    multi-command

--- Serial_sample/Serial_sample.ino
+++ Serial_sample/Serial_sample.ino
@@ -1,14 +1,29 @@
+# define command_len 2
+
 int get_t;
 bool flag;
+int state;
+int command[command_len];
 
 void get_serial(int get_t){
   Serial.println(get_t);
+}
+
+void print_command(int len){
+  for (int i=0; i<command_len; i++){
+    Serial.print(command[i]);
+    Serial.print(' ');
+  }
+  Serial.print('\n');
 }
 
 void setup() {
   Serial.begin(9600);
   get_t = 0;
   flag = false;
+  state = 0;
+  for (int i=0; i<command_len; i++)
+    command[i] = 0;
 }
 
 void loop() {
@@ -17,8 +32,15 @@ void loop() {
     if (get_char == ' ' || get_char == '\n' || get_char == '\r'){
       if (flag){
         flag = false;
-        get_serial(get_t);
+        if (state < command_len){
+          command[state] = get_t;
+          state++;
+        }
         get_t = 0;
+        if (get_char == '\n' || get_char == '\r'){
+          print_command(command_len);
+          state = 0;
+        }
       }
    }
    else{
